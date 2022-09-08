a287165936bbfb6ef85a48c60dac774f08f22748
Apurv Vinay Bidkar <97932925+apurvbidkar@users.noreply.github.com>
2022-09-08 21:16:36 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
--#!/bin/sh
-ssh root@44.201.194.30<<EOF
-   cd /var/www/html/
+#!/bin/sh
+ssh root@44.201.194.30 < cd /var/www/html/
    git pull origin master
    npm install --production
    pm2 restart all
