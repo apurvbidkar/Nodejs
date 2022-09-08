1d46dd229aaeca8c7e1515879ef3565bae5f9332
Apurv Vinay Bidkar <97932925+apurvbidkar@users.noreply.github.com>
2022-09-08 21:10:54 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
-#!/bin/sh     
-ssh jenkins@ip-172-31-22-83 <  cd /home/ubuntu/
- git pull      
- npm install --production      
- pm2 restart all
- exit      
+#!/bin/sh
+ssh root@44.201.194.30<<EOF
+   cd /var/www/html/
+   git pull origin master
+   npm install --production
+   pm2 restart all
+   exit
 EOF
