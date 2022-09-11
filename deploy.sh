cb8869a235a715ab2301b173761375f1d80c2399
Apurv Vinay Bidkar <97932925+apurvbidkar@users.noreply.github.com>
2022-09-11 23:40:38 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-ssh ubuntu@44.211.47.216 < cd /home/ubuntu/Nodejs
+ssh -T ubuntu@44.211.47.216 < cd /home/ubuntu/Nodejs
    git pull origin master
    npm install --production
    pm2 restart all
